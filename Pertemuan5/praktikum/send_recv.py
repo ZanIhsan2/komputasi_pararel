9b03b47108714f12e5520834a99ef56ae4713249
ZanIhsan2 <rasyid1234rismawan@gmail.com>
2025-10-26 22:06:42 +0700

    pertemuan5-updated

---
+++ Pertemuan5/praktikum/send_recv.py
@@ -0,0 +1,10 @@
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+if rank == 0:
+    data = "Hallo dari proses 0"
+    comm.send(data, dest=1)
+    print(f"[{rank}] Mengirim data: {data}")
+elif rank == 1:
+    data = comm.recv(source=0)
+    print(f"[{rank}] Menerima data: {data}")
